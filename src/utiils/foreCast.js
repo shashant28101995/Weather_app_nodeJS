5048ed3140f482019e7dd1edaed8e717c4e619f3
shashant <shashant.bartwal@evontech.com>
2019-07-01 12:01:06 +0530

    chnages in weather data

--- src/utiils/foreCast.js
+++ src/utiils/foreCast.js
@@ -3,16 +3,16 @@ const foreCast = (longitude, latitude, callBack) => {
     const url = 'https://api.darksky.net/forecast/d8f5035a1f6786a817a52c9bd8de6492/' + latitude + ',' + longitude + '?units=si'
     request({ url: url, json: true }, (err, { body }) => {
 
-        const { error, currently,hourly } = body
-        console.log("BOdy ",JSON.stringify(body))
+        const { error, currently, daily } = body
+        console.log("BOdy ", JSON.stringify(body))
         if (err) {
             callBack('Some ErrorOccurs networks', undefined)
         } else if (error) {
             callBack('Some ErrorOccurs', undefined)
         } else {
-            callBack(undefined, "The temperature is " + currently.temperature + "`c .the chance of rain is " +
-                changeStringToFloat(currently.precipProbability) + "%. "+hourly.summary)
-        
+            callBack(undefined, daily.summary+"The temperature is " + currently.temperature + "`c ."+" The maximum temperature will be "+daily.data[0].temperatureHigh
+            + "`c and minimum will be "+daily.data[0].temperatureLow+ "`c. The chance of rain is " +changeStringToFloat(currently.precipProbability) + "%. ")
+
         }
 
     })
