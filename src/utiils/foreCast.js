0dff6b60fb80725ebda097f5f4d75552c39b9663
shashant <shashant.bartwal@evontech.com>
2019-07-01 11:40:31 +0530

    change in message from server

--- src/utiils/foreCast.js
+++ src/utiils/foreCast.js
@@ -2,14 +2,16 @@ const request = require('request');
 const foreCast = (longitude, latitude, callBack) => {
     const url = 'https://api.darksky.net/forecast/d8f5035a1f6786a817a52c9bd8de6492/' + latitude + ',' + longitude + '?units=si'
     request({ url: url, json: true }, (err, { body }) => {
-        const { error, currently } = body
+
+        const { error, currently,hourly } = body
+        console.log("BOdy ",JSON.stringify(body))
         if (err) {
             callBack('Some ErrorOccurs networks', undefined)
         } else if (error) {
             callBack('Some ErrorOccurs', undefined)
         } else {
             callBack(undefined, "The temperature is " + currently.temperature + "`c .the chance of rain is " +
-                changeStringToFloat(currently.precipProbability) + "%.")
+                changeStringToFloat(currently.precipProbability) + "%. "+hourly.summary)
         
         }
 
